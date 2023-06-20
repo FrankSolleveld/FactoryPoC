66321a1cdd9294fe959774c6c2441b932b179322
Frank Solleveld <frank.solleveld@nslogin.nl>
2023-06-20 09:56:45 +0200

    Initial Commit

---
+++ FactoryPoC/FactoryPoCApp.swift
@@ -0,0 +1,17 @@
+//
+//  FactoryPoCApp.swift
+//  FactoryPoC
+//
+//  Created by Frank Solleveld on 20/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct FactoryPoCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
